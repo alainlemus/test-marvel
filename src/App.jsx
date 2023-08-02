4e8222a23e4a1d6331fba6d8b9b8474d8c676c93
alainink <alain@inkwonders.com>
2023-08-02 00:23:00 -0600

    correcion en instalacionde tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='card'>Vite + React</h1>
+      <h1 className='text-sm text-blue'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-2xl'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{html,js}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
