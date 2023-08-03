1b233927e0bcd63dcd8da7fcb2c9e0b1df5abc6c
alainink <alain@inkwonders.com>
2023-08-03 10:48:09 -0600

    comentarios del cleinte de subapbase

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,5 +1,5 @@
 import {createClient} from '@supabase/supabase-js'
-//tuve un error para cargar desde el .env
+//son mis credenciales de supabase, reemplazar por las que se ocupen aqui o en un achivo .env.local
 export const client = createClient(
     'https://vokqmptgcbtvfhcqdweq.supabase.co', 
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZva3FtcHRnY2J0dmZoY3Fkd2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEwMTExNjAsImV4cCI6MjAwNjU4NzE2MH0.fpMejlki0Xs73g-7IEviqxPa6mmN1WjZ4PJ7KMA1Efg'
